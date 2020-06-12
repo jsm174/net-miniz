44ad532b3e43291b37c765d8fa5e843e37d0b052
jsm174 <jsm174@gmail.com>
2020-06-12 18:36:25 -0400

    Updated tests to use 'using' so streams automatically close

--- Tests/UtilsTest.cs
+++ Tests/UtilsTest.cs
@@ -15,19 +15,20 @@ namespace Tests
         {
             var utils = new NetMiniZUtils();
 
-            var outStream = new FileStream("Test_compressed.png", FileMode.Create, FileAccess.Write, FileShare.Read);
-            var inStream = new FileStream("Test.png", FileMode.Open, FileAccess.Read, FileShare.Read);
-            utils.Compress(inStream, outStream, 6);
+            using (var outStream = new FileStream("Test_compressed.png", FileMode.Create, FileAccess.Write, FileShare.Read))
+                using (var inStream = new FileStream("Test.png", FileMode.Open, FileAccess.Read, FileShare.Read))
+                    utils.Compress(inStream, outStream, 6);
 
-            outStream = new FileStream("Test_decompressed.png", FileMode.Create, FileAccess.Write, FileShare.Read);
-            inStream = new FileStream("Test_compressed.png", FileMode.Open, FileAccess.Read, FileShare.Read);
-            utils.Decompress(inStream, outStream);
+            using (var outStream = new FileStream("Test_decompressed.png", FileMode.Create, FileAccess.Write, FileShare.Read))
+                using (var inStream = new FileStream("Test_compressed.png", FileMode.Open, FileAccess.Read, FileShare.Read))
+                    utils.Decompress(inStream, outStream);
 
-            var s1Info = new FileInfo("Test.png");
-            var s2Info = new FileInfo("Test_decompressed.png");
-
-            if (!File.ReadAllBytes(s1Info.FullName).SequenceEqual(
-                 File.ReadAllBytes(s2Info.FullName)))
+            if (File.ReadAllBytes(new FileInfo("Test.png").FullName).SequenceEqual(
+                 File.ReadAllBytes(new FileInfo("Test_decompressed.png").FullName)))
+            {
+                Console.WriteLine("Test_decompressed.png matches Test.png");
+            }
+            else
             {
                 throw new Exception("Test_decompressed.png does not match Test.png");
             }
